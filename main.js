628e60ae14759d3314bc7d045d1e6854334a68b4
d-McGill <dmcgillz@icloud.ciom>
2021-01-17 14:24:24 +0000

    adding js

---
+++ main.js
@@ -0,0 +1,15 @@
+
+
+let project document.getElementById('projects');
+let commercial document.getElementById('commercial');
+let research document.getElementById('research');
+let fun document.getElementById('fun');
+
+
+project.addEventListener('click' , clickFunction);
+
+
+
+function clickFunction(){
+  
+}
