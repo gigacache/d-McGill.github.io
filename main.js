3ebc27c25ce296c9939b3dbc0fbcdc6b0b3efd7c
d-McGill <dmcgillz@icloud.com>
2021-08-20 12:21:41 +0200

    -c

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ var speed = 50;
 function typeWriter() {
   var txt = 'Ready To Launch Currently studying BSc (Hons) Software Engineering at Manchester Metropolitan University. Passionate and hard working with a creative twist, looking for an opportunity to develop current skills and grow as an individual in the technology industry. Sociable personality and a keen eye for detail, an effective team player. Willingness to learn and will always take on board feedback to progress. Also I love to eat pizza and skateboard.';
   if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
+    document.getElementById("intro").innerHTML += txt.charAt(i);
     i++;
     setTimeout(typeWriter, speed);
   }
