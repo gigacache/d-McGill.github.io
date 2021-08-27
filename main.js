d93b10454191307cbb30be01f80e120307bdc603
d-McGill <dmcgillz@icloud.com>
2021-08-27 14:12:24 +0200

    nearly complete

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var i = 0;
 var speed = 50;
 
 function typeWriter() {
-  var txt = 'Ready To Launch Currently studying BSc (Hons) Software Engineering at Manchester Metropolitan University. Passionate and hard working with a creative twist, looking for an opportunity to develop current skills and grow as an individual in the technology industry. Sociable personality and a keen eye for detail, an effective team player. Willingness to learn and will always take on board feedback to progress. Also I love to eat pizza and skateboard.';
+  var txt = 'In my final year of studying BSc (Hons) Software Engineering at Manchester Metropolitan University. Passionate and hard working with a creative twist, looking for an opportunity to develop current skills and grow as an individual in the technology industry. Sociable personality and a keen eye for detail, an effective team player. Willingness to learn and will always take on board feedback to progress. Also I love to eat pizza and skateboard.';
   if (i < txt.length) {
     document.getElementById("intro").innerHTML += txt.charAt(i);
     i++;
