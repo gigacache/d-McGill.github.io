b5915a679b6710f7be3961634ecaf35108585147
d-McGill <dmcgillz@icloud.com>
2021-09-02 11:36:25 +0200

    -c

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ var i = 0;
 var speed = 50;
 
 function typeWriter() {
-  var txt = 'No Signal - Dont forget to scroll';
+  var txt = 'just remember where on a floating rock in space';
   if (i < txt.length) {
     document.getElementById("intro").innerHTML += txt.charAt(i);
     i++;
